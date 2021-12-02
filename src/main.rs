93425965f9c8918330dfb17645b3d548188104c0
vedranvinko <vinkovedran@gmail.com>
2021-12-02 09:31:21 +0100

    Add cursive and root element

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use cursive::align::HAlign;
+use cursive::event::Key;
+use cursive::traits::*;
+use cursive::views::{Dialog, TextView};
+use cursive::Cursive;
+
 fn main() {
-    println!("Hello, world!");
+    let mut root = cursive::default();
+
+    root.add_global_callback(Key::Esc, |s| s.quit());
+
+    root.run();
 }
