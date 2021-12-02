dc1f1be5f2d4884d0f86daadc1fa782770a97ba7
vedranvinko <vinkovedran@gmail.com>
2021-12-02 14:46:29 +0100

    Send GET and deserialize

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,56 @@ use cursive::views::{Dialog, EditView, LinearLayout, TextView};
 use cursive::Cursive;
 use cursive_async_view::{AsyncState, AsyncView};
 
+use reqwest::header::{ACCEPT, USER_AGENT};
 use serde::{Deserialize, Serialize};
 
 use std::time::{Duration, Instant};
 
 #[derive(Debug, Deserialize, Serialize)]
 struct User {
-    login: String,
-    public_repos: String,
-    avatar_url: String,
+    login: Option<String>,
+    id: Option<u32>,
+    node_id: Option<String>,
+    avatar_url: Option<String>,
+    gravatar_id: Option<String>,
+    url: Option<String>,
+    html_url: Option<String>,
+    followers_url: Option<String>,
+    following_url: Option<String>,
+    gists_url: Option<String>,
+    starred_url: Option<String>,
+    subscriptions_url: Option<String>,
+    organizations_url: Option<String>,
+    repos_url: Option<String>,
+    events_url: Option<String>,
+    received_events_url: Option<String>,
+    type_: Option<String>,
+    site_admin: Option<bool>,
+    name: Option<String>,
+    company: Option<String>,
+    blog: Option<String>,
+    location: Option<String>,
+    email: Option<String>,
+    hireable: Option<bool>,
+    bio: Option<String>,
+    twitter_username: Option<String>,
+    public_repos: Option<u32>,
+    public_gists: Option<u32>,
+    followers: Option<u32>,
+    following: Option<u32>,
+    created_at: Option<String>,
+    updated_at: Option<String>,
 }
 
 #[tokio::main]
 async fn get_user_info(name: &str) -> Result<User, reqwest::Error> {
     let url = format!("https://api.github.com/users/{}", name);
-    let rsp = reqwest::get(url).await?;
+    let rsp = reqwest::Client::new()
+        .get(url)
+        .header(USER_AGENT, "vedranvinko")
+        .header(ACCEPT, "application/vnd.github.v3+json")
+        .send()
+        .await?;
     let body = rsp.text().await?;
     let user: User = serde_json::from_str(&body).unwrap();
 
