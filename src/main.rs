1016c6b45007b8c4a6caa0d6233ee9c3dfa15e33
vedranvinko <vinkovedran@gmail.com>
2021-12-02 09:45:50 +0100

    Add input field and next view

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use cursive::align::HAlign;
 use cursive::event::Key;
 use cursive::traits::*;
-use cursive::views::{Dialog, TextView};
+use cursive::views::{Dialog, EditView, TextView};
 use cursive::Cursive;
+
+fn show_info(root: &mut Cursive, name: &str) {
+    root.pop_layer();
+
+    let text = format!("User {}", name);
+
+    root.add_layer(Dialog::around(TextView::new(text)).button("Finish", |s| s.quit()));
+}
 
 fn main() {
     let mut root = cursive::default();
+
+    root.add_layer(
+        Dialog::new()
+            .title("Enter Github user")
+            .padding_lrtb(1, 1, 1, 0)
+            .content(EditView::new().on_submit(show_info).with_name("name")),
+    );
 
     root.add_global_callback(Key::Esc, |s| s.quit());
 
